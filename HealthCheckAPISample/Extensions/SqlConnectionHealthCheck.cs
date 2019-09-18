56f6e3e00372a2584a8e75d1fd3bf550eb8faeae
Thanh Le <letienthanh0212@gmail.com>
2019-09-18 22:10:07 +0700

    Initial project
    first commit

---
+++ HealthCheckAPISample/Extensions/SqlConnectionHealthCheck.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthCheckAPISample.Extensions
+{
+    // Sample SQL Connection Health Check
+    public class SqlConnectionHealthCheck:IHealthCheck
+    {
+        private static readonly string DefaultTestQuery = "Select 1";
+        public string ConnectionString { get; }
+        public string TestQuery { get; }
+        public SqlConnectionHealthCheck(string connectionString)
+            : this(connectionString, testQuery: DefaultTestQuery)
+        {
+        }
+        public SqlConnectionHealthCheck(string connectionString, string testQuery)
+        {
+            ConnectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+            TestQuery = testQuery;
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync(cancellationToken);
+
+                    if (TestQuery != null)
+                    {
+                        var command = connection.CreateCommand();
+                        command.CommandText = TestQuery;
+
+                        await command.ExecuteNonQueryAsync(cancellationToken);
+                    }
+                }
+                catch (DbException ex)
+                {
+                    return new HealthCheckResult(status: context.Registration.FailureStatus, exception: ex);
+                }
+            }
+
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
---
+++ HealthCheckAPISample/Startup.cs
@@ -0,0 +1,67 @@
+﻿using HealthCheckAPISample.Extensions;
+using HealthChecks.System;
+using HealthChecks.UI.Client;
+using HealthChecks.UI.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthCheckAPISample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddHealthChecks()
+                .AddDiskStorageHealthCheck(delegate (DiskStorageOptions diskStorageOptions)
+                {
+                    diskStorageOptions.AddDrive(@"C:\", 100);
+                }, name: "My Drive", HealthStatus.Unhealthy)
+                .AddSqlServer(Configuration["ConnectionStrings:DefaultConnection"]);
+            //.AddCheck("My Database", new SqlConnectionHealthCheck(Configuration["ConnectionStrings:DefaultConnection"])); 
+            services.AddHealthChecksUI();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            // HealthCheck middleware
+            app.UseHealthChecks("/hc", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+            app.UseHealthChecksUI(delegate (Options options)
+            {
+                options.UIPath = "/hc-ui";
+                options.AddCustomStylesheet("./Customization/custom.css");
+            });
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
